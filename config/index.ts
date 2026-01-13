104ca341d19098ae0c3e3c9a3adbe4e3966d6162
yuan <2206529229@qq.com>
2026-01-13 17:36:15 +0800

    ``` feat(config): 配置文件优化
    - 移除 linebreak-style 规则以适应不同操作系统
    - 根据 TARO_ENV 环境变量动态设置输出目录
    - 启用 Webpack 持久化缓存提升构建性能
    - 将 tsconfig-paths 插件配置移动到正确位置
    ```

--- config/index.ts
+++ config/index.ts
@@ -17,7 +17,7 @@ export default defineConfig<"webpack5">(async (merge, { command, mode }) => {
       828: 1.81 / 2,
     },
     sourceRoot: "src",
-    outputRoot: "dist",
+    outputRoot: `dist/${process.env.TARO_ENV}`,
     plugins: [],
     defineConstants: {},
     copy: {
@@ -27,10 +27,11 @@ export default defineConfig<"webpack5">(async (merge, { command, mode }) => {
     framework: "react",
     compiler: "webpack5",
     cache: {
-      enable: false, // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
+      enable: true, // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
     },
     mini: {
       webpackChain(chain, webpack) {
+        chain.resolve.plugin("tsconfig-paths").use(TsconfigPathsPlugin);
         chain.merge({
           plugin: {
             install: {
@@ -58,9 +59,6 @@ export default defineConfig<"webpack5">(async (merge, { command, mode }) => {
             generateScopedName: "[name]__[local]___[hash:base64:5]",
           },
         },
-      },
-      webpackChain(chain) {
-        chain.resolve.plugin("tsconfig-paths").use(TsconfigPathsPlugin);
       },
     },
     h5: {
